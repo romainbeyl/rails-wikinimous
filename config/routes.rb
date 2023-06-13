fdae8e799b46a2f1e6d74ef96ba72e4e54d630aa
romain beyl <beyl-romain@hotmail.fr>
2023-06-13 16:58:58 +0200

    002

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/edit'
+  get 'articles/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles
 end
